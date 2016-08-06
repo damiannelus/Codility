0aa36910d38920f8a69afe4c7cf4968e47672c40
Y50-70 <Y50-70@Lenovo>
2016-08-06 16:09:15 +0200

    Fina 100% PermMissingElem

--- Codility/src/PermMissingElem.java
+++ Codility/src/PermMissingElem.java
@@ -1,12 +1,18 @@
+import java.util.Arrays;
 
 public class PermMissingElem {
 	public static void main(String[] args) {
-		int[] A = {2,3,1,5};
+//		int[] A = {2,3,4,1};
+		int[] A = {};
 		System.out.println(findMissing(A));
 	}
 
 	private static int findMissing(int[] A) {
-		// TODO Auto-generated method stub
-		return 0;
+		if(A.length == 0) return 1;
+		Arrays.sort(A);
+		for (int i = 0; i < A.length; i++) {
+			if(A[i] != i+1) return i+1;
+		}
+		return A[A.length-1]+1;
 	}
 }
