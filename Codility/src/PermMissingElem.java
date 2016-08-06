8803559a4f34f16c2e0c110d013d7ada102d6b70
Y50-70 <Y50-70@Lenovo>
2016-08-06 15:55:49 +0200

    Initial PermMissingElem

---
+++ Codility/src/PermMissingElem.java
@@ -0,0 +1,12 @@
+
+public class PermMissingElem {
+	public static void main(String[] args) {
+		int[] A = {2,3,1,5};
+		System.out.println(findMissing(A));
+	}
+
+	private static int findMissing(int[] A) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
