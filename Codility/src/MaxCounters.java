950aa873784a0d2dd9d00e8a674d1dde4a14e4f3
Y50-70 <Y50-70@Lenovo>
2016-08-07 00:17:39 +0200

    Initial MaxCounters

---
+++ Codility/src/MaxCounters.java
@@ -0,0 +1,17 @@
+
+public class MaxCounters {
+	public static void main(String[] args) {
+		int[] A = {3,3,3,6,1,4,4}; int N=5;
+		int[] result = getCounters(A,N);
+		for (int i = 0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+		
+	}
+
+	private static int[] getCounters(int[] A, int N) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
