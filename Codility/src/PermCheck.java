6b65433d0f863e15eabcbc5a62273c97c0dc3a19
Y50-70 <Y50-70@Lenovo>
2016-08-06 19:15:33 +0200

    100% PermCheck

--- Codility/src/PermCheck.java
+++ Codility/src/PermCheck.java
@@ -1,4 +1,17 @@
+import java.util.Arrays;
 
 public class PermCheck {
+	public static void main(String[] args) {
+//		int[] A = {4,1,3,2};
+		int[] A = {4,1,3};
+		System.out.println(isPermutation(A));
+	}
 
+	private static int isPermutation(int[] A) {
+		Arrays.sort(A);
+		for (int i = 0; i < A.length; i++) {
+			if(i+1!=A[i]) return 0;
+		}
+		return 1;
+	}
 }
