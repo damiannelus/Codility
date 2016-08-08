8bf3a00d5cd101661dcf62d9026c864276de093f
Y50-70 <Y50-70@Lenovo.local>
2016-08-08 17:17:02 +0200

    PassingCars initial commit

---
+++ Codility/src/PassingCars.java
@@ -0,0 +1,20 @@
+
+public class PassingCars {
+	public static void main(String[] args) {
+		int[] A = {0,1,0,1,1}; //5
+//		int[] A = {0,0,0,0,1}; //1
+//		int[] A = {0,0,0,0,1}; //0
+//		int[] A = {0,0,0,0,1}; //1
+		
+		System.out.println(countPasses(A));
+	}
+
+	private static int countPasses(int[] A) {
+		int onesCounter = 0;
+		int completeCounter = 0;
+		for (int i = A.length-1; i >= 0; i--) {
+			
+		}
+		return -1;
+	}
+}
